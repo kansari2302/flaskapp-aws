c75b1e7fbd589561299688428ee8b61cdd613401
Khalid Ansari <kansari2302@gmail.com>
2025-07-28 00:02:39 +1000

    initial commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-echo "Installing Python3 and pip3 if missing..."
-sudo yum install -y python3-pip
-
-echo "Installing Flask..."
-pip3 install --user -r /home/ec2-user/flaskapp/requirements.txt
-
-echo "Dependencies installed successfully"
-echo "You can now start the Flask app using the start_server.sh script"
-echo "To stop the Flask app, use the stop_server.sh script"
-echo "Make sure to run these scripts with appropriate permissions"
-echo "For example, use 'sudo ./start_server.sh' or 'sudo ./stop_server.sh' if necessary"
-echo "Installation complete"
+cd /home/ec2-user/flaskapp
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
