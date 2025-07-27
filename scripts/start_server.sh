630a33ebffb2dc12c181624d05d7e8e79be9b8b6
Khalid Ansari <kansari2302@gmail.com>
2025-07-28 00:22:33 +1000

    Correct start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/flaskapp
+touch app.log
+chmod 666 app.log
 echo "Flask app started" | tee -a app.log
-nohup python3 /home/ec2-user/flaskapp/app.py  # >> /home/ec2-user/flaskapp/app.log 2>&1 &
+nohup python3 app.py >> app.log 2>&1 &
