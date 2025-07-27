a048c1a78cec0fd10366379aa0a46297ef49504e
Khalid Ansari <kansari2302@gmail.com>
2025-07-28 09:35:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -12,9 +13,11 @@ def index():
         return redirect(url_for("thankyou"))
     return render_template("index.html")
 
+
 @app.route("/thankyou")
 def thankyou():
     return "<h2>Thank you for your feedback!</h2><a href='/'>Back to Home</a>"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
